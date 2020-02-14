b714c94542596b59c0fcea26f507441a770d2ab2
Edilson <edytavares66@gmail.com>
2020-02-14 11:46:50 -0300

    folder struct

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import './DiecePage.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Jogo de dados'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
