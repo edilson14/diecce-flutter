5d0fed1b0648d977f24373d4ebe03ea5a24f7f92
edilson14 <edytavares66@gmail.com>
2020-02-16 17:58:03 -0300

    flatButton

--- lib/pages/DiecePage.dart
+++ lib/pages/DiecePage.dart
@@ -3,19 +3,29 @@ import 'package:flutter/material.dart';
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset(
-            'images/dice2.png',
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+              child: Image.asset(
+                'images/dice1.png',
+              ),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image.asset(
-            'images/dice6.png',
-          ),
-        )
-      ],
+          Expanded(
+            child: FlatButton(
+              onPressed: () => {},
+              child: Image.asset(
+                'images/dice2.png',
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
